3421f8ff3fcacf6c8f6e923627da7d2ccbb89b3f
Ryszard Sinius <ryszard.sinius@gmail.com>
2021-05-04 20:40:56 +0200

    List adapter

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.android_lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO: App
+        target = new ArrayList<>();
+        target.addAll(Arrays.asList("Pies", "kot", "koń", "gołąb"));
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, target);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 }
