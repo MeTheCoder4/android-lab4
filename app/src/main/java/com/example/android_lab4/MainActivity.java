201f0932b73c0a3675cd703a4e56c6f11c8f0c76
Ryszard Sinius <ryszard.sinius@gmail.com>
2021-05-05 21:32:50 +0200

    db added

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android_lab4;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
 
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+
+    private MySQLite db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +29,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         target = new ArrayList<>();
         target.addAll(Arrays.asList("Pies", "kot", "koń", "gołąb"));
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, target);
+        db = new MySQLite(this);
+        adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[]{"_id", "gatunek"},
+                new int[]{android.R.id.text1,
+                        android.R.id.text2},
+                        SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public void nowyWpis(MenuItem mi) {
-        Intent intencja = new Intent(this,
-                DodajWpis.class);
-        startActivityForResult(intencja, 1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            String nowy = (String) extras.get("wpis");
-            target.add(nowy);
-            adapter.notifyDataSetChanged();
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.main_menu, menu);
+            return true;
         }
 
+        public void nowyWpis (MenuItem mi){
+            Intent intencja = new Intent(this,
+                    DodajWpis.class);
+            startActivityForResult(intencja, 1);
+        }
+
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                String nowy = (String) extras.get("wpis");
+                target.add(nowy);
+                adapter.notifyDataSetChanged();
+            }
+
+        }
     }
-}
